1c4286081bae0cd21d7dcae3de9ed2462da76500
oxxide216 <vie.devr@gmail.com>
2025-08-27 22:10:00 +0300

    Added submodules

--- build-test.sh
+++ build-test.sh
@@ -1,29 +1,9 @@
 #!/usr/bin/sh
 
 CFLAGS="-Wall -Wextra -Isrc -Ilibs/winx -Ilibs"
-LDFLAGS="-z execstack"
+LDFLAGS="-z execstack -lX11 -lGL -lGLEW"
+BUILD_FLAGS="${@:1}"
 SRC="$(find src -name "*.c")"
-WINX_SRC_PATH="../winx/src"
-WINX_LIBS_PATH="libs/winx"
-LIBS="-lX11 -lGL -lGLEW"
+WINX_SRC="libs/winx/src"
 
-if [ "$1" == "--rebuild-libs" ]; then
-  BUILD_FLAGS="${@:2}"
-  if [ -d "libs" ]; then
-    rm -r libs
-  fi
-else
-  BUILD_FLAGS="${@:1}"
-fi
-
-if [ ! -d libs ]; then
-  mkdir libs
-fi
-
-if [ ! -d $WINX_LIBS_PATH ]; then
-  cp -r $WINX_SRC_PATH $WINX_LIBS_PATH
-fi
-
-LIBS_SRC="$(find libs -name "*.c")"
-
-cc -o glass-test $CFLAGS $LIBS $LDFLAGS $BUILD_FLAGS $SRC $LIBS_SRC
+cc -o glass-test $CFLAGS $LDFLAGS $BUILD_FLAGS $SRC $WINX_SRC
