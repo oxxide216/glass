4d1e7c3e430a34891ec670ca2c83ff4a0d06ccca
oxxide216 <vie.devr@gmail.com>
2025-08-27 22:16:51 +0300

    Fixed build script

--- build-test.sh
+++ build-test.sh
@@ -4,6 +4,6 @@ CFLAGS="-Wall -Wextra -Isrc -Ilibs/winx -Ilibs"
 LDFLAGS="-z execstack -lX11 -lGL -lGLEW"
 BUILD_FLAGS="${@:1}"
 SRC="$(find src -name "*.c")"
-WINX_SRC="libs/winx/src"
+WINX_SRC="$(find libs/winx/src -name "*.c")"
 
 cc -o glass-test $CFLAGS $LDFLAGS $BUILD_FLAGS $SRC $WINX_SRC
